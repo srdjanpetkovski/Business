5ca0f708512091d9278269f7836c7d60dfc9c773
srdjanpetkovski <srdjanpetkovski@gmail.com>
2021-11-06 10:21:35 +0100

    header html

--- index.php
+++ index.php
@@ -1,50 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Business</title>
-    <link rel="stylesheet" href="scss/style.css">
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
-    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;600;700&display=swap" rel="stylesheet">
-</head>
-<body>
-    <!-- nav -->
-    <nav>
-        <a href="index.php" class="logo">BUSINESS <br> <span>Marketing &amp; consulting</span></a>
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="about.php">About</a></li>
-            <li><a href="services.php">Services</a></li>
-            <li><a href="team.php">Team</a></li>
-            <li><a href="#">Sign up</a></li>
-            <li><a href="#"><i class="fas fa-search"></i></a></li>
-        </ul>
-    </nav>
+<?php include "partials/header.php"; ?>
 
-    <!-- footer -->
-    <footer>
-        <div class="container">
-            <div class="col">
-                <a href="index.php" class="logo">BUSINESS <br> <span>Marketing &amp; consulting</span></a>
-            </div>
-            <div class="col">
-                <i class="fas fa-map-marker-alt"></i>
-                <p>Main street 2, Novi Sad</p>
-                <p>Serbia</p>
-            </div>
-            <div class="col">
-                <i class="fas fa-envelope"></i>
-                <p>Main street 2, Novi Sad</p>
-                <p>Serbia</p>
-            </div>
-            <div class="col">
-                <i class="fas fa-phone"></i>
-                <p>Main street 2, Novi Sad</p>
-                <p>Serbia</p>
-            </div>
-        </div>
-    </footer>
-</body>
-</html>
+    <!-- header -->
+    <header>
+        <h1>Trusted and <br> professionals advisers</h1>
+        <h3>For your business</h3>
+        <p>Showcase your Profile to the world using online CV builder and Get Hired Faster</p>
+        <button>About us</button>
+    </header>
+
+<?php include "partials/footer.php"; ?>
---
+++ partials/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Business</title>
+    <link rel="stylesheet" href="scss/style.css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;600;700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <!-- nav -->
+    <nav>
+        <a href="index.php" class="logo">BUSINESS <br> <span>Marketing &amp; consulting</span></a>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="services.php">Services</a></li>
+            <li><a href="team.php">Team</a></li>
+            <li><a href="#">Sign up</a></li>
+            <li><a href="#"><i class="fas fa-search"></i></a></li>
+        </ul>
+    </nav>
